94fa7a207fec2b61d9bd32e40514d2bad836f487
Carlos-Pujols <carlospujols94@gmail.com>
2023-01-11 21:26:21 -0400

    Hamburger menu script added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const menuButton = document.getElementById('menu-btn');
 const navInfo = document.getElementById('nav-info');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
 menuButton.addEventListener('click',() =>{
     navInfo.classList.toggle('active-menu');
+    line1.classList.toggle('line-1');
+    line2.classList.toggle('line-2');
+    line3.classList.toggle('line-3');
+
 })
