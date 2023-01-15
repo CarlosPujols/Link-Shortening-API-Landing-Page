1d241350bb04492d5f996c85aec4ad8957854456
Carlos-Pujols <carlospujols94@gmail.com>
2023-01-15 13:16:19 -0400

    Generated form validation and DOM changes when input is empty

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const line3 = document.getElementById('line3');
 const ctaButton = document.getElementById('cta-button');
 const results = document.getElementById('results-container');
 const shortenResultContainer = document.getElementById('shorten-result-container')
+const linkInput = document.getElementById('link');
+const mainCta = document.getElementById('main-cta');
+
 menuButton.addEventListener('click',() =>{
     navInfo.classList.toggle('active-menu');
     line1.classList.toggle('line-1');
@@ -16,36 +19,49 @@ menuButton.addEventListener('click',() =>{
 
 ctaButton.addEventListener('click', (event)=>{
     event.preventDefault();
+    if (linkInput.value == ""){
+        document.getElementById('input-error').classList.add('visible');
+        linkInput.classList.add('main-cta-error');
+    }else{
+        document.getElementById('input-error').classList.remove('visible');
+        linkInput.classList.remove('main-cta-error');
+        const longUrl = document.getElementById('link').value;
+        const targetUrl = `https://api.shrtco.de/v2/shorten?url=${longUrl}`;
 
-    const longUrl = document.getElementById('link').value;
-    const targetUrl = `https://api.shrtco.de/v2/shorten?url=${longUrl}`;
+        fetch(targetUrl)
+        .then((res) => {
+            if (!res.ok){
+                throw new Error("Invalid link");
+            }
+            return res.json();
+            })
+        .then((data) => {
+            if(data.ok){
+            const longLink = data.result.original_link;
+            const shortLink = data.result.full_short_link2;
 
-    fetch(targetUrl)
-    .then((res) => res.json())
-    .then((data) => {
+            let resultModuleChilds = document.querySelectorAll('#shorten-result-container .copy-button').length;
+            
+            let resultModule = `<div class="shorten-result" id="shorten-result-container">
+            
+            <div class="shorten-result-info">
+            <p class="long-link" id="long-link${resultModuleChilds + 1}">${longLink}</p>
+            <p class="shortened-link" id="shortened-link${resultModuleChilds + 1}">${shortLink}</p>
+            </div>
+            <button class="copy-button" id="copy-button${resultModuleChilds + 1}">Copy</button>
+            </div>`;
 
-        const longLink = data.result.original_link;
-        const shortLink = data.result.full_short_link2;
+            results.innerHTML += resultModule;
 
-        let resultModuleChilds = document.querySelectorAll('#shorten-result-container .copy-button').length;
-        
-        let resultModule = `<div class="shorten-result" id="shorten-result-container">
-        
-        <div class="shorten-result-info">
-        <p class="long-link" id="long-link${resultModuleChilds + 1}">${longLink}</p>
-        <p class="shortened-link" id="shortened-link${resultModuleChilds + 1}">${shortLink}</p>
-        </div>
-        <button class="copy-button" id="copy-button${resultModuleChilds + 1}">Copy</button>
-        </div>`;
-
-        results.innerHTML += resultModule;
-
-        results.classList.add('visible-result');
+            results.classList.add('visible-result');
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });  
     }
-);
 
     
-
 });
 
 results.addEventListener('click', (event) => {
